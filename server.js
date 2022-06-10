489e0877a5a570ed06654922187ae6912cb369ad
Alexander <alexander.gotra@gmail.com>
2022-06-10 10:29:17 +0300

    changed cors origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 var corsOptions = {
-  origin: 'https://quiet-reef-78598.herokuapp.com',
+  origin: 'https://quiet-reef-78598.herokuapp.com/api/tutorials',
 };
 
 app.use(cors(corsOptions));
